151722c5f1b7a5ca48f0ecc4ac7bc6d72e1798e3
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-10-02 20:25:27 +0200

    commit
    Signed-off-by: Tiebe Groosman <tiebe.groosman@gmail.com>

--- magisterapi/build.gradle.kts
+++ magisterapi/build.gradle.kts
@@ -113,8 +113,15 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/OpenbaarLyceumZeist/MagisterAPIKt")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            components.forEach {
+                from(it)
             }
         }
     }
