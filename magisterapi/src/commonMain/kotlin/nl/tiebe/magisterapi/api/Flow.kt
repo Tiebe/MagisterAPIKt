e20d3a4ac6240f79eba72e2ec0cdaa6fddcbaa05
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-10-27 14:28:31 +0200

    dk

--- magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/api/Flow.kt
+++ magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/api/Flow.kt
@@ -7,6 +7,7 @@ import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
 import nl.tiebe.magisterapi.utils.MagisterException
 
@@ -22,7 +23,8 @@ private val client = HttpClient {
 suspend fun requestPOST(
     url: Url,
     body: HashMap<String, String> = hashMapOf(),
-    accessToken: String? = null
+    accessToken: String? = null,
+    retries: Int = 0
 ): HttpResponse {
 
 
@@ -40,8 +42,17 @@ suspend fun requestPOST(
 
     response.status.let {
         if (it != HttpStatusCode.OK) {
-            throw MagisterException(response.status, response.bodyAsText(),
-                "Request failed with status code ${it.value} with message ${response.bodyAsText()}")
+            if (response.bodyAsText().contains("<HTML><HEAD>") && retries < 30) {
+                println("Retrying #${retries + 1}...")
+                delay(2000*(retries+1).toLong())
+
+                return requestPOST(url, body, accessToken, retries + 1)
+            } else {
+                throw MagisterException(
+                    response.status, response.bodyAsText(),
+                    "Request failed with status code ${it.value} with message ${response.bodyAsText()}"
+                )
+            }
         }
     }
 
@@ -51,7 +62,8 @@ suspend fun requestPOST(
 suspend fun requestPOST(
     url: Url,
     requestBody: Any,
-    accessToken: String? = null
+    accessToken: String? = null,
+    retries: Int = 0
 ): HttpResponse {
 
 
@@ -66,8 +78,17 @@ suspend fun requestPOST(
 
     response.status.let {
         if (it != HttpStatusCode.OK) {
-            throw MagisterException(response.status, response.bodyAsText(),
-                "Request failed with status code ${it.value} with message ${response.bodyAsText()}")
+            if (response.bodyAsText().contains("<HTML><HEAD>") && retries < 30) {
+                println("Retrying #${retries + 1}...")
+                delay(2000*(retries+1).toLong())
+
+                return requestPOST(url, requestBody, accessToken, retries + 1)
+            } else {
+                throw MagisterException(
+                    response.status, response.bodyAsText(),
+                    "Request failed with status code ${it.value} with message ${response.bodyAsText()}"
+                )
+            }
         }
     }
 
@@ -77,28 +98,41 @@ suspend fun requestPOST(
 suspend fun requestGET(
     url: Url,
     body: HashMap<String, String> = hashMapOf(),
-    accessToken: String? = null
+    accessToken: String? = null,
+    retries: Int = 0
 ): HttpResponse {
-
-    val response = client.get(url) {
-        url {
-            parameters.apply {
-                for ((key, value) in body) {
-                    append(key, value)
+    try {
+        val response = client.get(url) {
+            url {
+                parameters.apply {
+                    for ((key, value) in body) {
+                        append(key, value)
+                    }
                 }
+            }
+
+            if (accessToken != null) {
+                bearerAuth(accessToken)
             }
         }
 
-        if (accessToken != null) {
-            bearerAuth(accessToken)
-        }
-    }
+        response.status.let {
+            if (it != HttpStatusCode.OK) {
+                if (response.bodyAsText().contains("<HTML><HEAD>") && retries < 30) {
+                    println("Retrying #${retries + 1}...")
+                    delay(2000*(retries+1).toLong())
 
-    response.status.let {
-        if (it != HttpStatusCode.OK) {
-            throw MagisterException(response.status, response.bodyAsText(),
-                "Request failed with status code ${it.value} with message ${response.bodyAsText()}")
+                    return requestGET(url, body, accessToken, retries + 1)
+                } else {
+                    throw MagisterException(
+                        response.status, response.bodyAsText(),
+                        "Request failed with status code ${it.value} with message ${response.bodyAsText()}"
+                    )
+                }
+            }
         }
+    } catch (e: IllegalArgumentException) {
+        if
     }
 
     return response
