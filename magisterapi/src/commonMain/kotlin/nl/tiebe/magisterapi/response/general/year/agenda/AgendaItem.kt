6d6add1a42d282b5cf450144d4d187717d4b936a
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-11-02 23:32:17 +0100

    agenda shit

---
+++ magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/response/general/year/agenda/AgendaItem.kt
@@ -0,0 +1,95 @@
+package nl.tiebe.magisterapi.response.general.year.agenda
+import kotlinx.serialization.Serializable
+
+import kotlinx.serialization.SerialName
+import nl.tiebe.magisterapi.response.general.year.Classroom
+import nl.tiebe.magisterapi.response.general.year.Subject
+
+
+@Serializable
+data class AgendaItem(
+    @SerialName("Id")
+    val id: Int,
+    @SerialName("Links")
+    val links: List<Link>,
+    @SerialName("Start")
+    val start: String,
+    @SerialName("Einde")
+    val einde: String,
+    @SerialName("LesuurVan")
+    val fromPeriod: Int,
+    @SerialName("LesuurTotMet")
+    val untilPeriod: Int,
+    @SerialName("DuurtHeleDag")
+    val lastsAllDay: Boolean,
+    @SerialName("Omschrijving")
+    val description: String,
+    @SerialName("Lokatie")
+    val location: String,
+    @SerialName("Status")
+    val status: Int,
+    @SerialName("Type")
+    val type: Int,
+    @SerialName("Subtype")
+    val subtype: Int,
+    @SerialName("IsOnlineDeelname")
+    val isOnline: Boolean,
+    @SerialName("WeergaveType")
+    val displayType: Int,
+    @SerialName("Inhoud")
+    val content: String,
+    @SerialName("InfoType")
+    val infoType: Int,
+    @SerialName("Aantekening")
+    val note: String?,
+    @SerialName("Afgerond")
+    val finished: Boolean,
+    @SerialName("HerhaalStatus")
+    val repeatingState: Int,
+    @SerialName("Herhaling")
+    val repeating: Repeating,
+    @SerialName("Vakken")
+    val vakken: List<Subject>,
+    @SerialName("Docenten")
+    val teachers: List<Teacher>,
+    @SerialName("Lokalen")
+    val classrooms: List<Classroom>,
+    @SerialName("Groepen")
+    val groups: String?,
+    @SerialName("OpdrachtId")
+    val assignmentId: Int,
+    @SerialName("HeeftBijlagen")
+    val hasAttachments: Boolean,
+    @SerialName("Bijlagen")
+    val attachments: String?
+) {
+    companion object {
+        @Serializable
+        data class Link(
+            @SerialName("Rel")
+            val rel: String,
+            @SerialName("Href")
+            val href: String
+        )
+
+        @Serializable
+        data class Repeating(
+            @SerialName("BeginDatum")
+            val startingDate: String,
+            @SerialName("AantalKeer")
+            val numberOfTimes: Int,
+            @SerialName("Dagelijks")
+            val daily: Daily
+        )
+
+        @Serializable
+        data class Daily(
+            @SerialName("Interval")
+            val interval: Int
+        )
+
+        enum class Type(val type: Int) {
+            LESSON(1)
+        }
+    }
+}
--- magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/response/general/year/agenda/Teacher.kt
+++ magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/response/general/year/agenda/Teacher.kt
@@ -1,12 +1,14 @@
-package nl.tiebe.magisterapi.response.general.year.absence
+package nl.tiebe.magisterapi.response.general.year.agenda
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Subject(
+data class Teacher(
     @SerialName("Id")
     val id: Int,
     @SerialName("Naam")
-    val name: String?
+    val name: String,
+    @SerialName("Docentcode")
+    val teacherCode: String
 )
