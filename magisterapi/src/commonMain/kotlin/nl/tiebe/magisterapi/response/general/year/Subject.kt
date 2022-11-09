39b93936ddb4b21e49261a173f46a19319d6fef1
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-11-09 20:36:01 +0100

    agenda shit (#3)
    Co-authored-by: Gulp1n <abeldenouden@hotmail.com>

--- magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/response/general/year/Subject.kt
+++ magisterapi/src/commonMain/kotlin/nl/tiebe/magisterapi/response/general/year/Subject.kt
@@ -1,10 +1,12 @@
-package nl.tiebe.magisterapi.response.general.year.absence
+package nl.tiebe.magisterapi.response.general.year
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Classroom(
+data class Subject(
+    @SerialName("Id")
+    val id: Int,
     @SerialName("Naam")
     val name: String?
 )
