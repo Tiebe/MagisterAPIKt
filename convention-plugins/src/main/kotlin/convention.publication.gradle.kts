addffebaf7e02a072f925f76178667bd58700e59
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-07-10 20:19:30 +0200

    lets see if its faster

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -40,6 +40,9 @@ val javadocJar by tasks.registering(Jar::class) {
 
 fun getExtraString(name: String) = ext[name]?.toString()
 
+
+
+
 publishing {
     // Configure maven central repository
     repositories {
@@ -66,7 +69,7 @@ publishing {
     publications.withType<MavenPublication> {
 
         // Stub javadoc.jar artifact
-        artifact(javadocJar.get())
+        //artifact(javadocJar.get())
 
         // Provide artifacts information requited by Maven Central
         pom {
