9a615bf4c82ff222493ef7e9c990b3ec46fb6381
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-11-23 20:52:52 +0100

    abcd

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("com.android.tools.build:gradle:8.0.2")
     }
 }
