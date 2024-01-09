a0b819e05a5b3aa53541580fc02acecca611baad
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-01-09 21:49:35 +0100

    update deps

--- magisterapi/build.gradle.kts
+++ magisterapi/build.gradle.kts
@@ -43,16 +43,16 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.3.7"
+        val ktorVersion = "2.0.3"
         val kryptoVersion = "3.4.0"
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
                 implementation("com.benasher44:uuid:0.4.1")
                 implementation("com.soywiz.korlibs.krypto:krypto:$kryptoVersion")
                 implementation("io.matthewnelson.kotlin-components:encoding-base64:1.1.3")
