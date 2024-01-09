dfc8db9a0aacefef1485229a130a26d2d23e65a1
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-01-09 22:18:13 +0100

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.android.tools.build:gradle:8.2.1")
     }
 }
 
