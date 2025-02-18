2c3d29184c1d34603b2b4570904e82294927f4d1
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-02-18 20:03:12 +0100

    Native (#30)
    * Add native support
    
    * Remove libraries
    
    * Little less random, but that's alright
    
    * Ooops
    
    * Change version

--- magisterapi/src/commonMain/kotlin/dev/tiebe/magisterapi/utils/LoginUtility.kt
+++ magisterapi/src/commonMain/kotlin/dev/tiebe/magisterapi/utils/LoginUtility.kt
@@ -1,25 +1,26 @@
 package dev.tiebe.magisterapi.utils
 
-import com.soywiz.krypto.SHA256
-import com.soywiz.krypto.SecureRandom
-import io.ktor.utils.io.core.*
-import io.matthewnelson.component.base64.Base64
-import io.matthewnelson.component.base64.encodeBase64
-import kotlinx.datetime.Clock
 import dev.tiebe.magisterapi.response.TokenResponse
+import io.ktor.utils.io.core.*
+import kotlinx.datetime.Clock
+import org.kotlincrypto.hash.sha2.SHA256
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.random.Random
 
 object LoginUtility {
+    @OptIn(ExperimentalEncodingApi::class)
     fun generateCodeVerifier(): String {
         val codeVerifier = ByteArray(32)
-        SecureRandom.nextBytes(codeVerifier)
-        return codeVerifier.encodeBase64(base64 = Base64.UrlSafe(pad = false))
+        Random.nextBytes(codeVerifier)
+        return Base64.UrlSafe.withPadding(Base64.PaddingOption.ABSENT).encode(codeVerifier)
     }
 
+    @OptIn(ExperimentalEncodingApi::class)
     fun generateCodeChallenge(codeVerifier: String): String {
         val bytes: ByteArray =
             codeVerifier.toByteArray()
-
-        return SHA256.digest(bytes).bytes.encodeBase64(base64 = Base64.UrlSafe(pad = false))
+        return Base64.UrlSafe.withPadding(Base64.PaddingOption.ABSENT).encode(SHA256().digest(bytes))
     }
 
     fun checkExpired(expiresAt: Long): Boolean {
